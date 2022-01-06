d526baac9f6343f74caf43cdd27f8abce555e42b
Sofiedige <sofiedige1@gmail.com>
2022-01-06 17:03:36 +0100

    cup class

--- src/main/java/fields/FerrySquare.java
+++ src/main/java/fields/FerrySquare.java
@@ -1,6 +1,6 @@
 package fields;
 
-public class FerrySquare extends Square { // Creates our VisitJailSquare field, and which functionality wise doesn't do anything
+public class FerrySquare extends Square { // Creates our FerrySquare field, and which functionality wise doesn't do anything
     public FerrySquare(String fieldName) {
         super(fieldName);
     }
